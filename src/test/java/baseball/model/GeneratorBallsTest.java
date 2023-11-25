c6b2144d7850fd549d8153fa41d540ad8c47ff1b
jongheonleee <qwefghnm1212@gmail.com>
2023-11-25 11:17:21 +0900

    [test] GeneratorBalls 클래스 테스트

---
+++ src/test/java/baseball/model/GeneratorBallsTest.java
@@ -0,0 +1,26 @@
+package baseball.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class GeneratorBallsTest {
+
+    GeneratorImp generator = new GeneratorImp();
+    GeneratorBalls generatorBalls = new GeneratorBalls(generator);
+
+    @DisplayName("1~9까지 중복을 허용하지 않고 서로 다른 임의의 수 생성 기능 테스트")
+    @RepeatedTest(5)
+    void generateBallsTest() {
+        List<Integer> balls = generatorBalls.generateUniqueNumbers();
+        Set<Integer> ballsSet = balls.stream().collect(Collectors.toSet());
+        Boolean result = balls.size() == ballsSet.size();
+        assertEquals(true, result);
+    }
+
+}
