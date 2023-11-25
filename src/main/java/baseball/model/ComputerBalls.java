41dd746fdf9d0c16e47bcc231f73a36fe06d3df9
jongheonleee <qwefghnm1212@gmail.com>
2023-11-25 11:30:16 +0900

    [feat] ComputerBalls 클래스 구현

--- src/main/java/baseball/model/ComputerBalls.java
+++ src/main/java/baseball/model/ComputerBalls.java
@@ -1,5 +1,48 @@
 package baseball.model;
 
+import baseball.view.InputView;
+import java.util.Collections;
+import java.util.List;
+
 public class ComputerBalls {
 
+    private static final Integer MIN_NUMBER = 1;
+    private static final Integer MAX_NUMBER = 9;
+
+    private static final Integer LENGTH = 3;
+
+    private final List<Integer> balls;
+
+    public ComputerBalls(List<Integer> balls) {
+        validate(balls);
+        this.balls = balls;
+    }
+
+    private void validate(List<Integer> balls) {
+        if (isIncludedInRangeNumbers(balls)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 값입니다.");
+        }
+
+        if (isAvailableLength(balls)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 길이입니다.");
+        }
+
+    }
+
+    private Boolean isIncludedInRangeNumbers(List<Integer> balls) {
+        return balls.stream()
+                .allMatch(ball -> isIncludedInRangeNumber(ball));
+    }
+
+    private Boolean isIncludedInRangeNumber(Integer ball) {
+        return (MIN_NUMBER <= ball && ball <= MAX_NUMBER);
+    }
+
+    private Boolean isAvailableLength(List<Integer> balls) {
+        return LENGTH == balls.size();
+    }
+
+    public List<Integer> getBalls() {
+        return Collections.unmodifiableList(balls);
+    }
 }
