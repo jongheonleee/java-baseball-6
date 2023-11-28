8d328ac2c238bb49d96a65672ca833637f964b80
jongheonleee <qwefghnm1212@gmail.com>
2023-11-28 14:13:14 +0900

    [chore] 게임 종료 문구 출력

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -56,6 +56,7 @@ public class GameController {
     }
 
     private Boolean restartGame() {
+        outputView.printGameEnd();
         Integer answer = inputView.requestRestart();
         return answer == 1 ? true : false;
     }
